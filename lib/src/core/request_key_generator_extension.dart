afeaeda8095ade7bcb9dbc4a7a338dcbb562a2bf
Abdurahman Ibrahem <56024526+AbdurahmanAlmehdi@users.noreply.github.com>
2025-06-10 12:54:55 +0200

    Update request_key_generator_extension.dart

--- lib/src/core/request_key_generator_extension.dart
+++ lib/src/core/request_key_generator_extension.dart
@@ -11,9 +11,12 @@ extension RequestKeyGeneratorExtension on RequestOptions {
     final queryParams = jsonEncode(Map.fromEntries(
         queryParameters.entries.toList()
           ..sort((a, b) => a.key.compareTo(b.key))));
-    final headers = jsonEncode(Map.fromEntries(this.headers.entries.toList()
-      ..where((element) => allowedHeaders.contains(element.key))
-      ..sort((a, b) => a.key.compareTo(b.key))));
+    final ogHeaders = this.headers.entries.toList();
+    final filteredHeaders = ogHeaders
+        .where((element) => allowedHeaders.contains(element.key.toLowerCase()))
+        .toList();
+    final headers = jsonEncode(Map.fromEntries(
+        filteredHeaders..sort((a, b) => a.key.compareTo(b.key))));
     final body = data != null ? jsonEncode(data) : '';
     final rawKey = '$method|$path|$queryParams|$headers|$body';
     final bytes = utf8.encode(rawKey);
