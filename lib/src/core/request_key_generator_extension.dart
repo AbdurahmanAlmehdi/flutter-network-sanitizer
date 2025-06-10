e8bf373cf62a441d4cf5a90609d73480044fd789
AbdurahmanAlmehdi <abdurahman.i.work@gmail.com>
2025-06-10 02:41:13 +0200

    fix

--- lib/src/core/request_key_generator_extension.dart
+++ lib/src/core/request_key_generator_extension.dart
@@ -4,17 +4,16 @@ import 'package:crypto/crypto.dart' show sha256;
 import 'package:dio/dio.dart';
 
 extension RequestKeyGeneratorExtension on RequestOptions {
-  const allowedHeaders = ['authorization', 'accept-language'];
+  static const allowedHeaders = ['authorization', 'accept-language'];
   String get generateRequestKey {
     final method = this.method.toUpperCase();
     final path = uri.toString();
     final queryParams = jsonEncode(Map.fromEntries(
         queryParameters.entries.toList()
           ..sort((a, b) => a.key.compareTo(b.key))));
-    final headers = jsonEncode(Map.fromEntries(
-        this.headers.entries.toList()
-          ..where((element) => allowedHeaders.contains(element.key))
-          ..sort((a, b) => a.key.compareTo(b.key))));
+    final headers = jsonEncode(Map.fromEntries(this.headers.entries.toList()
+      ..where((element) => allowedHeaders.contains(element.key))
+      ..sort((a, b) => a.key.compareTo(b.key))));
     final body = data != null ? jsonEncode(data) : '';
     final rawKey = '$method|$path|$queryParams|$headers|$body';
     final bytes = utf8.encode(rawKey);
